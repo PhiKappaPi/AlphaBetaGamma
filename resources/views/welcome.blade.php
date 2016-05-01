d04619aedb5f2e2882873a26ebead795b783cc9e
kkroli <kevin.krolicki@mail.mcgill.ca>
2016-05-01 15:27:56 -0400

    FIRST

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>pkp dev stuff</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <link href="{{URL::asset('css/app.css')}}" rel="stylesheet" type="text/css">
+		
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+				<div class="title">ΦΚΠ</div>
+				<div class="back-box"><h2 class="back-title">Phi Kappa Pi</h2></div>
+			</div>
+			<div class="content img-content">
+				<img class="home-crest" src="{{URL::asset('img/Phi_Kappa_Pi_Fraternity_Crest.png')}}" alt='Crest'>
+			</div>
+			<div class="content light-blue"> 	              
+				<div class="chapters-row">
+					<div class="chapters">
+						<H2>ΣΠ</H2>
+						<H5 class= "years">1903</H5>
+					 </div>
+					 <div class="chapters">
+						<H2>ABΓ</H2>
+						<H5 class= "years">1905</H5>
+					</div>
+					<div class="chapters">
+						<H2>ZΓ</H2>
+						<H5 class= "years">1923</H5>
+					</div> 
+					<div class="chapters">
+						<H2>ΩE</H2>
+						<H5 class= "years">2012</H5>
+					</div>
+				</div>
+			</div>
+        </div>
+    </body>
+</html>
