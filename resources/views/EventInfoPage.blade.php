c3638102c1aa9a274f86c42db9cb69a793fa8f51
Bacon <jdcf50@gmail.com>
2016-05-01 16:02:43 -0400

    Set up of event page

---
+++ resources/views/EventInfoPage.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Golfing Fundraiser</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <link href="{{URL::asset('css/app.css')}}" rel="stylesheet" type="text/css">
+
+</head>
+</html>
