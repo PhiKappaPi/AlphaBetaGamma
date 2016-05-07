6293da996365817eadf551e97167a194b2eae3e0
Bacon <jdcf50@gmail.com>
2016-05-06 22:01:55 -0400

    added photo and info text

--- resources/views/EventInfoPage.blade.php
+++ resources/views/EventInfoPage.blade.php
@@ -22,8 +22,8 @@
 						<h2 style="font-size: 100px; margin-top: -70px;padding-bottom:10px;">ANNUAL GOLF TOURNAMENT</h2>
 					</div>
 				</div>
-				<div class="content" >
-					<div style="align: center; padding: 1px 600px 30px 600px; font-size:30px; background-color:#202c4a; ">
+				<div class="content" style="height: auto" >
+					<div style="align: center; padding: 1px 300px 30px 300px; font-size:30px; background-color:#202c4a; ">
 					<p>
 						The Alumni Society would like to invite you to the long-lasting tradition of the 2016 PKP Annual Golf Tournament in Arundel, Quebec happening on June 11th, 2016. For years, this event has brought all brothers back to an environment of comradery and joy, allowing everyone to hop on back to memory lane on that single day. 
 						</p>
@@ -32,59 +32,74 @@
 						</p>
 						We have many activities open to actives and alumni; you can choose to partake in one or all of them. 
 					</div>
-					<div style="align:left; width: 1200px; height: 1500px; font-size: 30px; text-align: left; padding-left:600px; width: 1000px">
-					<!Bacon is a real cool dude who knows stuff about lots of things>
-						<p>
-						Here are the highlights with expected costs:
-						</p>
-						<p>
-						Accommodations:<br>
-						•	We have put together group rates for reservations at ‘Le Grand Lodge Mont-Tremblant’ for June 10th and June 11th,<br>
-						•	Double lake-view suites from $195 per night + taxes<br>
-						•	Double Deluxe suites from $205 per night + taxes<br>
-						</p>
-						<p>
-						Golf:<br>
-						•	Arundel Golf & Country Club<br>
-						•	12 foursomes with first tee-off at 9:30 AM, and every 8 minutes thereafter<br>
-						•	For green fees + half cart (obligatory): Actives $50, Alumni $75<br>
-						</p>
-						<p>
-						Other Activities:<br>
-						•	For those choosing not to golf, the Mont Tremblant resort area offers an array of activities for the family:<br>
-						o	Relaxation: massages and Scandinavian baths<br>
-						o	Outdoors: hiking or biking<br>
-						o	Motorized: airplane/helicopter tours, karting, boat rentals<br>
-						•	You may also choose to relax at Jim's:<br>
-						o	Boating will be available
-						o	Help out Team Eileen with dinner preparations<br>
-						o	Volunteer to babysit for mothers going for massages and/or baths
-						</p>
-						<p>
-						BBQ Dinner at Jim's:<br>
-						•	Relaxing at Brother Jim Ross'; who is hosting the dinner at his cottage beside a lake, minutes from the golf course.<br>
-						•	The Works: steak and baked potatoes, Caesar salad, stir-fried mushrooms, coleslaw, potato salad, fresh bread and more desserts you can count<br>
-						•	Actives $25 (free if you help with preparations and cleanup), Alumni $40
-						</p>
-						<p>
-						Let’s make sure to thank Brother Jim Ross and his family for sharing their home and hosting this great event!
-						</p>
-						<p>
-						What you need to do:
-						</p>
-						<p>
-						-	Which activities or activity you would like to partake in <br>
-						-	Your foursome (golf)<br>
-						-	Remind your brothers, close or far, of the golf tournament (we want to see everone!)<br>
-						</p>
-						<p>
-						If you have any questions please private message Dereck Pickford and Sebastien Jacques 
-						</p>
-						<p>
-						Much love as always,
-						</p>
-						Dereck and Seby
-						</p>
+					<div >
+				
+						<img src="{{URL::asset('img/CoolDoods.jpg')}}"s style="float:right ;padding-left:30px; height: 500px; margin-right:160px; padding:20px 20px 20px 20px;margin-top:10px;; background-color:#202c4a; border-style: solid; border-width:2px"> </img>
+					
+						<div style="align:left; width: auto; height: auto; font-size: 28px; text-align: left;padding-left: 200px; width: 700px;padding-top:10px; margin-right:40px; margin-bottom: 100px">
+									<!WHAT WHERE WHEN HOW-MUCH>
+							<p>
+							Here are the highlights with expected costs:
+							</p>
+							<dl>
+							What
+							<dt>• 12 foursomes with first tee-off at 9:30 AM</dt> 
+							<dd>- Every 8 minutes thereafter </dd>
+							<dt>• Green fees + obligatory half cart: </dt>
+							<dd>- Actives $50</dd>
+							<dd>- Alumni $75</dd>
+							</dl>
+							<dl>
+							Accommodations
+							<dt>• Le Grand Lodge Mont-Tremblant	</dt>
+							<dt>• June 10 and June 11</dt>
+							<dt>• Group rates: </dt>
+							<dd>Double lake-view suites from $195 per night + taxes</dd>
+							<dd>Double Deluxe suites from $205 per night + taxes</dd>
+							</dt>
+							
+						</div>
+					</div>
+					<div>
+								<p style="align:center; font-size:30px">
+								Other Activities
+								</p>
+								
+								<p style="font-size:25px">For the Family</p>
+								<p>  Relaxation: massages and Scandinavian baths<br>
+								Outdoors: hiking or biking<br>
+								Motorized: airplane/helicopter tours, karting, boat rentals<br>
+								</p>
+								<p>You may also choose to relax at Jim's <br>
+								Boating will be available<br>
+								Help out Team Eileen with dinner preparations<br>
+								Volunteer to babysit for mothers going for massages and/or baths<br>
+								</p>
+								<p>
+								BBQ Dinner at Jim's:<br>
+								•	Relaxing at Brother Jim Ross'; who is hosting the dinner at his cottage beside a lake, minutes from the golf course.<br>
+								•	The Works: steak and baked potatoes, Caesar salad, stir-fried mushrooms, coleslaw, potato salad, fresh bread and more desserts you can count<br>
+								•	Actives $25 (free if you help with preparations and cleanup), Alumni $40
+								</p>
+								<p>
+								Let’s make sure to thank Brother Jim Ross and his family for sharing their home and hosting this great event!
+								</p>
+								<p>
+								What you need to do:
+								</p>
+								<p>
+								-	Which activities or activity you would like to partake in <br>
+								-	Your foursome (golf)<br>
+								-	Remind your brothers, close or far, of the golf tournament (we want to see everone!)<br>
+								</p>
+								<p>
+								If you have any questions please private message Dereck Pickford (DPickford@ca.loreal.com) and Sebastien Jacques (sebastien.jacqu@gmail.com)
+								</p>
+								<p>
+								Much love as always,
+								</p>
+								Dereck and Seby
+								</p>
 					</div>
 				</div>
 
