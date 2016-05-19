7e0671e41811853a05687346df8433e67f7cbab0
Bacon <jdcf50@gmail.com>
2016-05-18 22:12:31 -0400

    Added translucent banner via rgba command, might want to put it onto the bottom one too and move the photo down?
    Added columns to the information towards the bottom.

--- resources/views/EventInfoPage.blade.php
+++ resources/views/EventInfoPage.blade.php
@@ -13,8 +13,10 @@
 	<body class="bodystyle">
 	<div class="main-container">
 		<div class="event-title-box" > 
-			<h4 style="font-size: 50px; padding-top: 20px">Join us at the Phi Kappa Pi</h4>
+			<div class=text-container>
+			<h4 style="font-size: 50px; padding-top: 20px">PHI KAPPA PI</h4>
 			<h2 style="font-size: 100px; padding-bottom:10px;">ANNUAL GOLF TOURNAMENT</h2>
+			</div>
 		</div>
 		<div class="container-fluid about-container">
 			<div class="container">
@@ -32,70 +34,75 @@
 			</div>
 		</div>
 
-	<div class="container" style="text-align:center;display:table-cell;height:500px;background-color:#0d1e41">
+		<div class="container" style="text-align:center;display:table-cell;height:500px;background-color:#0d1e41; width:auto">
          
 
 		<aside >
-						<img src="{{URL::asset('img/CoolDoods.jpg')}}" style="float:right ;padding-left:30px; height: 500px; margin-right:500px; padding:20px 20px 20px 20px;margin-top:10px; background-color:#202c4a; border-style: solid; border-width:2px">
+						<h1 class="primary-headers">Location Information</h1>
+						<img src="{{URL::asset('img/CoolDoods.jpg')}}" style="float:right; height: 500px; margin: 0px 500px 10px 0px; padding:20px 20px 20px 20px; background-color:#202c4a; border-style: solid; border-width:2px">
 		</aside>
-		<section>
-				<div style="align:left; width: 1000px; font-size: 27px; text-align: left; padding-left: 500px; width: 700px;padding-top:10px; margin-right:50px; margin-bottom: 100px">
-										<!WHAT WHERE WHEN HOW-MUCH>
-								<h3>
-								Highlights & Expected Costs
-								</h3>
-								<dl>
-								Where:
-								<dt> - Golfing : Arundel Golf & Country Club</dt>
-								<dt> - Sleeping : Le Grand Lodge Mont-Tremblant</dt>
-								</dl>
-								<dl>
-								When:
-								<dt>- June 10 and 11 </dt>
-								<dt>- Golfing On June 11</dt>
-								<dd>· 12 foursomes</dd>
-								<dd>· First tee-off at 9:30 AM</dd> 
-								<dd>· Every 8 minutes thereafter </dd>
-								</dl>
-								<dl>Costs:
-								<dt>- Green Fees + Obligatory Half Cart: </dt>
-								<dd>· Alumni $75</dd>
-								<dd>· Actives $50</dd>
-								<dt>- Accommodations:</dt>
-								<dd>· Group rates</dd>
-								<dd>· Double Deluxe suites from $205 per night + taxes</dd>
-								<dd>· Double lake-view suites from $195 per night + taxes</dd>
-
-								</dl>
+		<section class="info">
+				<div class="main">
 								
+								<dl>
+									Where
+									<dt> Golfing : Arundel Golf & Country Club</dt>
+									<dt> Sleeping : Le Grand Lodge Mont-Tremblant</dt>
+								</dl>
+								<dl>
+								When
+									<dt>June 10 and 11 </dt>
+									<dt>Golfing On June 11</dt>
+									<dd>12 foursomes</dd>
+									<dd>First tee-off at 9:30 AM</dd> 
+									<dd>Every 8 minutes thereafter </dd>
+								</dl>
+				</div>
+				<div>
+								<h1 class="primary-headers">Costs</h1>
+								<dl>
+									<dt>Green Fees + Obligatory Half Cart: </dt>
+									<dd>Alumni $75</dd>
+									<dd>Actives $50</dd>
+									<dt>Accommodations:</dt>
+									<dd>Group rates</dd>
+									<dd>Double Deluxe suites from $205 per night + taxes</dd>
+									<dd>Double lake-view suites from $195 per night + taxes</dd>
+								</dl>
+				</div>
+				<div class="sub">
 			
-								<h3 >
-								Other Activities
-								</h3>
-								<dl>
-								<h5>For the Family</h5>
-								<dt>  Relaxation: massages and Scandinavian baths</dt>
-								<dt>Outdoors: hiking or biking</dt>
-								<dt>Motorized: airplane/helicopter tours, karting, boat rentals</dt>
-								</p>
-								</dl>
-								<h5>You May Also Choose To Relax At Jim's</h5>
-								<p>
-								Boating will be available<br>
-								Help out Team Eileen with dinner preparations<br>
-								Volunteer to babysit for mothers going for massages and/or baths<br>
-								Brother Jim Ross is hosting the dinner at his cottage beside a lake, minutes from the golf course.<br>
-								</p>
-								<dl>
-								<h5>BBQ Dinner at Jim's:</h5>
-								<dt>The Works! 
-								<dd>Steak and baked potatoes </dd>
-								<dd>Caesar salad, stir-fried mushrooms, coleslaw, potato salad</dd> 
-								<dd>Fresh bread and more desserts you can count</dd>
-								<dt>Reimbursement</dt>
-								<dd>Alumni $40</dd>
-								<dd>Actives $25 (free if you help with preparations and cleanup)</dd>
-								</dl>
+							<h1 class="primary-headers">Other Activities</h1>
+								<div class="col-md-4">
+									<dl>
+										<h5>For the Family</h5>
+										<dt>  Relaxation: massages and Scandinavian baths</dt>
+										<dt>Outdoors: hiking or biking</dt>
+										<dt>Motorized: airplane/helicopter tours, karting, boat rentals</dt>
+									</dl>
+								</div>
+								<div class="col-md-4">								
+									<h5>You May Also Choose To Relax At Jim's</h5>
+									<p>
+									Boating will be available<br>
+									Help out Team Eileen with dinner preparations<br>
+									Volunteer to babysit for mothers going for massages and/or baths<br>
+									Brother Jim Ross is hosting the dinner at his cottage beside a lake, minutes from the golf course.<br>
+									</p>
+								</div>
+								<div class="col-md-4">
+									<dl>
+										<h5>BBQ Dinner at Jim's:</h5>
+										<dt>The Works! 
+										<dd>Steak and baked potatoes </dd>
+										<dd>Caesar salad, stir-fried mushrooms, coleslaw, potato salad</dd> 
+										<dd>Fresh bread and more desserts you can count</dd>
+										<dt>Reimbursement</dt>
+										<dd>Alumni $40</dd>
+										<dd>Actives $25 (free if you help with preparations and cleanup)</dd>
+										</dt>
+									</dl>
+								</div>
 				</div>	
 		
 
@@ -121,12 +128,10 @@
 			<footer>
 						<h2 style="font-size: 70px; padding:20px 100px 50px 100px" >Let’s make sure to thank Brother Jim Ross and his family for sharing their home and hosting this great event!</h2>
 			</footer>
-				</div>
-			</div>
-		</div>
 		</div>
 	</div>
 
+	
 
 </body>
 </html>
