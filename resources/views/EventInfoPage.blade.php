721a36fd51cda269f958d21556a3f7418f5ae5c2
kkroli <kevin.krolicki@mail.mcgill.ca>
2016-05-18 20:40:56 -0400

    bootstrap

--- resources/views/EventInfoPage.blade.php
+++ resources/views/EventInfoPage.blade.php
@@ -5,6 +5,7 @@
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link href="{{URL::asset('css/app.css')}}" rel="stylesheet" type="text/css">
 	</head>
 <body class="bodystyle">
